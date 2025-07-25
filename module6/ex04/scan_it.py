a4a331471a48486e57e409e876b91f07dd8d41f1
pazlatov <ameliapaulazl3@gmail.com>
2025-07-25 09:07:54 +0200

    ex04

--- module6/ex04/scan_it.py
+++ module6/ex04/scan_it.py
@@ -3,3 +3,7 @@ import re
 if len(sys.argv) -1 != 2:
 	print("none")
 else:
+	if re.findall(rf'\b{re.escape(sys.argv[1])}\b', sys.argv[2]):
+		print(len(re.findall(rf'\b{re.escape(sys.argv[1])}\b', sys.argv[2])))
+	else:
+		print("none")
